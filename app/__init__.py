0eff50ec82ce83b3c5767c5a6b4b1eaf46c30d73
Steve Copley <steve.copley@waimea.school.nz>
2025-05-26 10:27:23 +1200

    Added a form handler

--- app/__init__.py
+++ app/__init__.py
@@ -7,31 +7,49 @@ from random import randint
 # Create the app
 app = Flask(__name__)
 
-
+# ------------------------------------------
+# Home page - loading a static page
 @app.get("/")
 def home():
     return render_template('pages/home.jinja')
 
+# ------------------------------------------
+# About page - loading a static page
 @app.get("/about/")
 def about():
     return render_template('pages/about.jinja')
 
+# ------------------------------------------
+# Random Number Page - passing a value into template
 @app.get("/random/")
 def random():
-    randNum = randint(1, 1000000000000000)
+    randNum = randint(1, 1000)
     return render_template('pages/random.jinja', number=randNum)
 
+# ------------------------------------------
+# Number Page - Getting a value from route
+#               and passing it into template
 @app.get("/number/<int:num>")
 def analyseNumber(num):
     print(f"You entered: {num}")
     return render_template('pages/number.jinja', number=num)
 
-
+# ------------------------------------------
+# Form Page - Static page with a form!
 @app.get("/form/")
 def form():
     return render_template('pages/form.jinja')
 
 
+# ------------------------------------------
+# Handle data posted from the form
+@app.post("/processForm")
+def processForm():
+    print(f"Form data: ${request.form}")
+
+
+# ------------------------------------------
+# Handle any missing pages
 @app.errorhandler(404)
 def notFound(e):
     return render_template("pages/404.jinja")
